b16411557bab6f8ffdec91efedf2dc28a1d3072b
Anand Biswas <anandmaya48@gmail.com>
2020-08-17 10:36:02 +0530

    home done

--- git/views.py
+++ git/views.py
@@ -5,22 +5,22 @@ from bs4 import BeautifulSoup
 def getUserDetail(username):
     session = get_session()
     url = "https://github.com/"+username
-    page=session.get(url)
+    page = session.get(url)
     profile_soup = BeautifulSoup(page.content, 'html.parser')
-    profile=profile_soup.find('div', class_="h-card mt-md-n5")
+    profile = profile_soup.find('div', class_="h-card mt-md-n5")
     profile_pic = profile.find('div', class_="position-relative d-inline-block col-2 col-md-12 mr-3 mr-md-0 flex-shrink-0").find('img')['src']
     name = profile.find('span', class_="p-name vcard-fullname d-block overflow-hidden").text
     user_name = profile.find('span', class_="p-nickname vcard-username d-block").text
-    data=[name, user_name, profile_pic]
+    data = [name, user_name, profile_pic]
     return data
 
 def getRepo(username):
     session = get_session()
     url = "https://github.com/"+username+"?tab=repositories"
-    page=session.get(url)
+    page = session.get(url)
     soup = BeautifulSoup(page.content, 'html.parser')
     repos = soup.find(id="user-repositories-list").find('ul').find_all('li')
-    data=[]
+    data = []
     for repo in repos:
         project = repo.find('div').find('div').find('h3').find('a')
         project_name = project.text
