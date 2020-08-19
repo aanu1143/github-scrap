eda81766800dfeffe72ab4a5c7062fdecb1600f4
Anand Biswas <anandmaya48@gmail.com>
2020-08-19 18:23:48 +0530

    pinned repo added

--- git/views.py
+++ git/views.py
@@ -16,15 +16,18 @@ def getUserDetail(username):
     return data
 
 def getFollow(username):
-    session = get_session()
-    url = "https://github.com/"+username
-    page = session.get(url)
-    profile_soup = BeautifulSoup(page.content, 'html.parser')
-    follows = profile_soup.find('div', class_="flex-order-1 flex-md-order-none mt-2 mt-md-0").find('div').find_all('a')
-    followers = x[0].find('span').text
-    followings = x[1].find('span').text
-    stars = x[2].find('span').text
-    return {"followers": followers, "followings": followings, "stars": stars}
+    try:
+        session = get_session()
+        url = "https://github.com/"+username
+        page = session.get(url)
+        profile_soup = BeautifulSoup(page.content, 'html.parser')
+        follows = profile_soup.find('div', class_="flex-order-1 flex-md-order-none mt-2 mt-md-0").find('div').find_all('a')
+        followers = follows[0].find('span').text
+        followings = follows[1].find('span').text
+        stars = follows[2].find('span').text
+        return {"followers": followers, "followings": followings, "stars": stars}
+    except:
+        return False
 
 def getRepo(username):
     session = get_session()
@@ -38,15 +41,44 @@ def getRepo(username):
         project_name = project.text
         project_link = 'https://github.com'+ project['href']
         des = repo.find('div').find_all('div')[1].find('p')
-        temp = {'projectname': project_name, 'link': project_link}
+        temp = {'name': project_name, 'link': project_link}
         if des:
-            temp['description'] = des.text
+            temp['des'] = des.text
         else:
-            temp['description'] = None
+            temp['des'] = None
         data.append(temp)
-
     return data
 
+def getPinnedRepo(username):
+    session = get_session()
+    url = "https://github.com/"+username
+    profile_page = session.get(url)
+    profile_soup = BeautifulSoup(profile_page.content, 'html.parser')
+    reposi = profile_soup.find('div', class_="js-pinned-items-reorder-container")
+    title = reposi.find('h2').text
+    repolist = reposi.find('ol').find_all('li')
+    reposlist = []
+    for y in repolist:
+        repo = {}
+        repo['name'] = y.find('span').text
+        repo['link'] = "https://github.com"+y.find('a')['href']
+        des = y.find_all('p')[0].text
+        langx = y.find('p', class_="mb-0 f6 text-gray").find('span', itemprop="programmingLanguage")
+        lang = ""
+        if langx:
+            lang = langx.text
+        forkedFlag = False
+        if "Forked" in des:
+            forkedFlag = True
+            forkedLink = "https://github.com" + repolist[-2].find_all('p')[0].find('a')['href']
+            forked = str(des)
+            des = y.find_all('p')[1].text
+            repo['forkdata'] = {'text': forked, 'link': forkedLink}
+        repo['lang'] = lang
+        repo['des'] = des
+        repo['fork'] = forkedFlag
+        reposlist.append(repo)
+    return {'title': title, 'repolist': reposlist}
 
 def index(request):
     return render(request, 'index.html')
@@ -54,10 +86,11 @@ def index(request):
 
 def gituser(request, username):
     userdetail = getUserDetail(username)
-    repos = getRepo(username)
+    repos = getPinnedRepo(username)
     follows = getFollow(username)
     data = {'name': userdetail[0], 'username': userdetail[1], 'img': userdetail[2], 'repos': repos}
-    data.update(follows)
+    if follows:
+        data.update(follows)
     return render(request, 'profile.html', data)
 
 # def gituser(request, username):
